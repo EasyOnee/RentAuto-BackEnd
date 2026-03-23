6661e91eb85de3bed49acc1646861172d2ac9584
Luis Peraza <warriorpropvp@hotmail.com>
2026-03-22 20:43:53 -0700

    cambios minimos

--- app/config/firebase.config.js
+++ app/config/firebase.config.js
@@ -1,4 +1,4 @@
-const admin = require('firebase-admin');
+/* const admin = require('firebase-admin');
 const serviceAccount = require('../../rentauto-dced6-firebase-adminsdk-d0ug3-7f6b73c66d.json');
 
 admin.initializeApp({
@@ -8,4 +8,15 @@ admin.initializeApp({
 
 const bucket = admin.storage().bucket();
 
-module.exports = { admin, bucket };
+module.exports = { admin, bucket }; */
+
+
+const admin = require('firebase-admin');
+
+// Railway pone la ruta absoluta en la variable de entorno
+const serviceAccount = require(process.env.FIREBASE_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'rentauto-dced6.appspot.com'
+});
